ba98ecff48b2fe1eb9c40a3f76c243ed4d4abf00
Jhoedy Indurain Dominguez Dominguez <72112653+JhoedyDominguez2201@users.noreply.github.com>
2021-02-05 13:43:32 -0500

    Add files via upload

--- lasilleria.js
+++ lasilleria.js
@@ -5,7 +5,7 @@ function traerdatos() {
 
     const xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', 'productos.json', true);
+    xhttp.open('GET', 'productos2.json', true);
     xhttp.send();
     xhttp.onload = function () {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
@@ -69,18 +69,93 @@ function expresionregular() {
     var telefono = document.getElementById("telefonoInput").value;
     var mensaje = document.getElementById("mensajeInput").value;
 
+    var pnombre = document.getElementById("p-nombre");
+    var papellido = document.getElementById("p-apellido");
+    var pemail = document.getElementById("p-email");
+    var ptelefono = document.getElementById("p-telefono");
+    var pmensaje  = document.getElementById("p-mensaje");
 
-    if (!(/^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])/.test(nombre))){
-        alert("Nombre invalido");
-       
+    var resultadonombre = false;
+    var resultadoapellido = false;
+    var resultadoemail = false;
+    var resultadotelefono = false;
+    var resutladomensaje = false;
+
+
+
+
+    if (!(/^([a-zA-Z ñáéíóú]{2,60})$/.test(nombre))) { 
+        pnombre.setAttribute("class", "text-danger")
+        pnombre.innerHTML = ("Nombre incorrecto");
+        resultadonombre = false;
+
     } else {
-        alert("Nombre valido");
-        
+        pnombre.setAttribute("class", "text-success")
+        pnombre.innerHTML = ("Nombre correcto");    
+        resultadonombre = true;
+
     }
-    if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))){
-        alert("email invalido");
-    } else {
-        alert("email valido");
+    if (!(/^([a-zA-Z ñáéíóú]{2,60})$/.test(apellido))) {
+        papellido.setAttribute("class", "text-danger")
+        papellido.innerHTML = ("Apellido incorrecto");
+        resultadoapellido = false;
 
+    } else {
+        papellido.setAttribute("class", "text-success")
+        papellido.innerHTML = ("Apellido correcto");
+        resultadoapellido = true;
+
+    }
+    if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))) {
+        pemail.setAttribute("class", "text-danger")
+        pemail.innerHTML = ("Email incorrecto");
+        resultadoemail = false;
+
+        
+    } else {
+        pemail.setAttribute("class", "text-success")
+        pemail.innerHTML = ("Email correcto");
+        resultadoemail = true;
+
+    }
+    if (!(/^(983|984|987|997|998)([0-9]){7}$/.test(telefono))) {
+        ptelefono.setAttribute("class", "text-danger")
+        ptelefono.innerHTML = ("Telefono incorrecto");
+        resultadotelefono = false;
+
+    } else {
+        ptelefono.setAttribute("class", "text-success")
+        ptelefono.innerHTML = ("Telefono correcto");
+        resultadotelefono = true;
+
+    }
+    if (!(/^([a-zA-Z ñáéíóú !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]{2,350})$/.test(mensaje))) {
+        pmensaje.setAttribute("class", "text-danger")
+        pmensaje.innerHTML = ("Mensaje incorrecto");
+        resutladomensaje = false;
+
+    } else {
+        pmensaje.setAttribute("class", "text-success")
+        pmensaje.innerHTML = ("Mensaje correcto");
+        resutladomensaje = true;
+    }
+
+    if(resultadonombre && resultadoapellido && resultadoemail && resultadotelefono && resutladomensaje){
+        swal({
+            title: "Mensaje enviado",
+            text: "Tu mensaje fue enviado con exito",
+            icon: "success",
+            button: "OK",
+            
+          });
+    }
+    else{
+        swal({
+            title: "Campo invalido",
+            text: "Verifica que todo este escrito correctamente",
+            icon: "error",
+            dangerMode: true,
+            button: "OK",
+          });
     }
 }
